4bed9d40fa9ecd5dd9b0cd3117955d9c2d417b87
Eugene Still <eugenestill@Eugenes-MacBook-Pro.local>
2023-03-13 18:00:36 -0700

    add trailing slash for 307 error

--- tests/test_users.py
+++ tests/test_users.py
@@ -17,15 +17,14 @@ def test_create_user(client):
 
 def test_login_user(test_user, client):
     res = client.post(
-        "/login", data={"username": test_user['email'], "password": test_user['password']})
-    # print(str(res.json()))
-    # login_res = schemas.Token(**res.json())
-    # payload = jwt.decode(login_res.access_token,
-    #                      settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-    # id = payload.get("user_id")
-    # assert id == test_user['id']
-    # assert login_res.token_type == "bearer"
-    assert res.status_code == 201 or res.status_code == 307
+        "/login/", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 201
 
 
 @pytest.mark.parametrize("email, password, status_code", [
@@ -37,6 +36,6 @@ def test_login_user(test_user, client):
 ])
 def test_incorrect_login(test_user, client, email, password, status_code):
     res = client.post(
-        "/login", data={"username": email, "password": password})
+        "/login/", data={"username": email, "password": password})
 
-    assert res.status_code == status_code or res.status_code == 307
+    assert res.status_code == status_code
